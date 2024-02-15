ab0173556f87bedf9bae6ec5ea85514dbb8052e7
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-02-15 01:29:37 +0100

    First android app with kotlin

---
+++ app/src/main/java/com/codad5/hellokotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codad5.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
