1a5373e5fba2061677505c2e90ceda7005db9caf
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2024-02-19 06:41:03 +0100

    Made a dice roller

--- app/src/main/java/com/codad5/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/codad5/hellokotlin/MainActivity.kt
@@ -4,27 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        findViewById<TextView>(R.id.textView2).text = ""
         val et : EditText = findViewById(R.id.editTextText)
-
-
         // button
         val btn : Button = findViewById(R.id.button)
 
         // handle click event
         btn.setOnClickListener {
             val editTextInput = et.text
-            val tv : TextView = findViewById(R.id.textView2)
-            tv.setText("Hello, $editTextInput")
             // get the text from the EditText
-            Toast.makeText(this@MainActivity , "Button Clicked", Toast.LENGTH_LONG).show()
+            Toast.makeText(this@MainActivity , "Hello $editTextInput", Toast.LENGTH_LONG).show()
+            rollDice()
         }
+
+        this.diceImage = findViewById(R.id.imageView)
+    }
+
+    private fun rollDice() {
+        val tv : TextView = findViewById(R.id.textView2)
+        val randomNumber = Random().nextInt(6 ) + 1
+        val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+        tv.text = "$randomNumber"
+        this.diceImage.setImageResource(drawableResource)
     }
 }
